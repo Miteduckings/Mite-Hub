12dd15527846eca173ffb1edf482546c167345bf
Miteduckings <117284868+Miteduckings@users.noreply.github.com>
2022-11-07 20:18:37 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,53 +1,78 @@
+if getgenv().Hub then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Mite Hub",
+        Text = "Mite Hub has already been executed in this game"
+    })
+    return
+end
+
+getgenv().Hub = true
+getgenv().supported = true
+getgenv().Check1 = true
+
+if require then
+    Check1 = true
+else
+    Check1 = false
+end
+
+
+if Check1 == true then
+    supported = true
+else
+    supported = false
+end
+
+local ownerId = game.CreatorId
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == ownerId then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Mite Hub",
+            Text = "Owner joined! be careful with what you do."
+        })
+    end
+end)
+
 if supported == true then
     local games = ({
-        [6847090259] = "bulkedup.lua",
-        [155615604] = "prisonlife.lua",
-        [4483381587] = "baseplate.lua",
-        [10243982775] = "redbox.lua",
-        [9498006165] = "tappingsimulator.lua",
-        [9872472334] = "evade.lua",
-        [10324346056] = "evade.lua",
-        [10324347967] = "evade.lua",
-        [10539706691] = "evade.lua",
-        [10662542523] = "evade.lua",
-        [10808838353] = "evade.lua",
-        [11353528705] = "evade.lua",
-        [11353532384] = "evade.lua",
+        [9872472334] = "Evade.lua",
+        [10324346056] = "Evade.lua",
+        [10324347967] = "Evade.lua",
+        [10539706691] = "Evade.lua",
+        [10662542523] = "Evade.lua",
+        [10808838353] = "Evade.lua",
+        [11353528705] = "Evade.lua",
+        [11353532384] = "Evade.lua",
     })[game.PlaceId]
     
     if games then
-        Rayfield:Destroy()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/Hydra-Network-V3/main/Games/" .. games))()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Miteduckings/Mite-Hub/main/Games/" .. games))()
     else
         game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "Hydra Network",
-            Text = "Hydra Network does not support this game"
+            Title = "Mite Hub",
+            Text = "Mite Hub does not support this game"
         })
     end
 end
 
 if supported == false then
     local gamez = ({
-        [6847090259] = "bulkedup.lua",
-        [155615604] = "prisonlife.lua",
-        [10243982775] = "redbox.lua",
-        [9498006165] = "tappingsimulator.lua",
-        [9872472334] = "evade.lua",
-        [10324346056] = "evade.lua",
-        [10324347967] = "evade.lua",
-        [10539706691] = "evade.lua",
-        [10662542523] = "evade.lua",
-        [10808838353] = "evade.lua",
-        [11353528705] = "evade.lua",
-        [11353532384] = "evade.lua",
+        [9872472334] = "Evade.lua",
+        [10324346056] = "Evade.lua",
+        [10324347967] = "Evade.lua",
+        [10539706691] = "Evade.lua",
+        [10662542523] = "Evade.lua",
+        [10808838353] = "Evade.lua",
+        [11353528705] = "Evade.lua",
+        [11353532384] = "Evade.lua",
     })[game.PlaceId]
     
     if gamez then
-        Rayfield:Destroy()
-        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/Hydra-Network-V3/main/Games/" .. gamez))()
+        loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Miteduckings/Mite-Hub/main/Games/" .. gamez))()
     else
         game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "Hydra Network",
-            Text = "Hydra Network does not support this game"
+            Title = "Mite Hub",
+            Text = "Mite Hub does not support this game"
         })
     end
+end
